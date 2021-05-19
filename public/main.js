1f46ba28a1bc06c4ebd3b9851ed0500d25646d26
Bianca Brown <biancakbrown58@gmail.com>
2021-05-19 14:19:11 -0400

    Able to add and subtract from both teams

--- public/main.js
+++ public/main.js
@@ -1,7 +1,50 @@
+let addToTeam1 = 0
+let addToTeam2 = 0
+
+function handleClick1(event) {
+  const buttonClicked = event.target
+
+  addToTeam1++
+  const team1Score = document.querySelector('.team1 h3')
+  team1Score.textContent = `${addToTeam1}`
+  // console.log(addToTeam1)
+
+  // console.log(team1Score)
+}
+
+function handleClick2(event) {
+  const buttonClicked = event.target
+  addToTeam2++
+  const team2Score = document.querySelector('.team2 h3')
+  team2Score.textContent = `${addToTeam2}`
+}
+
+function handleSub1(event) {
+  const buttonClicked = event.target
+  addToTeam1--
+  const team1Score = document.querySelector('.team1 h3')
+  team1Score.textContent = `${addToTeam1}`
+}
+function handleSub2(event) {
+  const buttonClicked = event.target
+  addToTeam2--
+  const team2Score = document.querySelector('.team2 h3')
+  team2Score.textContent = `${addToTeam2}`
+  // console.log(team2Score)
+}
+
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  const score1 = document.querySelector('.one.add')
+  score1.addEventListener('click', handleClick1)
+
+  const score2 = document.querySelector('.two.add')
+  score2.addEventListener('click', handleClick2)
+
+  const sub1 = document.querySelector('.one.subtract')
+  sub1.addEventListener('click', handleSub1)
+
+  const sub2 = document.querySelector('.two.subtract')
+  sub2.addEventListener('click', handleSub2)
 }
 
 document.addEventListener('DOMContentLoaded', main)
